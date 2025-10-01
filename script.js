412b520f9100e4e4383f9bc3b96264740922527a
jonnoble1979 <100154138+jonnoble1979@users.noreply.github.com>
2025-10-01 18:39:30 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-  document.querySelectorAll('.content-section').forEach(sec => sec.style.display = 'none');
-  document.getElementById(sectionId).style.display = 'block';
-}
-
-// Optionally, show the first section by default
-document.addEventListener('DOMContentLoaded', () => {
-  showSection('advice');
-});
